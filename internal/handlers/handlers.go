334551d537de6134ffdd1bb08444564825efcec8
Jack Rieck <jrieck@twilio.com>
2020-01-21 14:39:45 -0700

    add js classes

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -57,11 +57,11 @@ func route(s *server) {
 
 	// index
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "internal/web/index.html")
+		http.ServeFile(w, r, "web/index.html")
 	})
 
 	// js
-	r.PathPrefix("/static").Handler(http.StripPrefix("/static", http.FileServer(http.Dir("internal/web/js"))))
+	r.PathPrefix("/static").Handler(http.StripPrefix("/static", http.FileServer(http.Dir("web/js"))))
 
 	// api
 	r.HandleFunc("/server", s.getServer).Methods(http.MethodGet)
